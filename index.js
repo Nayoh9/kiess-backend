91898928e5ae93da3d06d03b41b5cb1e87d66fd2
Yohann Andre <y.andre90000@gmail.com>
2024-03-06 15:08:48 +0100

    add new adress local mongodb .env

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv').config()
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_LOCAL_URL)
+mongoose.connect("mongodb://127.0.0.1:27017/kiess")
 
 const app = express()
 app.use(express.json())
